13f1378729c90e28dcccb2113efa099e0185553d
toni19ab <55132207+toni19ab@users.noreply.github.com>
2021-12-13 09:33:31 +0100

    Update slidesFibonacci.js

--- slidesFibonacci.js
+++ slidesFibonacci.js
@@ -1,3 +1,6 @@
+//Obs. i den her fil er der to måder at løse opgaven på - som i kan se i videoen, så er den ene måde langt mere effektiv end den anden. Bosswords til den her opgave
+//og tilhørende løsning er HASHMAPS, GRAPHS, BIG O NOTATION (køretid)
+
 //Grundig forklaring af denne løsning i den her youtube video: https://www.youtube.com/watch?v=oBt53YbR9Kk&t=1996s   tid: ~3:30-38:40
 function fib(n) {
     if(n <= 2) {
